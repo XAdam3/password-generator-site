5453e92be5a1e812ab1e32b2aa11965f090ddb19
Adam <adamassalimy@gmail.com>
2022-10-23 16:27:55 -0700

    commit initial files

---
+++ index.js
@@ -0,0 +1,32 @@
+    function generatePass(){
+       
+    const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+
+    function randomElement (array) {
+    return array[Math.floor(Math.random() * array.length)]
+}
+
+    let randomPass = ""
+
+    let passwordlength = 12 
+    
+    for (let i = 0; i <= passwordlength; i++) {
+        
+        randomPass += randomElement(characters)
+        
+        }
+    
+        document.getElementById("inputOne").value = randomPass
+        document.getElementById("inputTwo").value = randomPass
+         
+         return randomPass
+      
+   }
+       
+
+
+     
+  
+  
+
+
